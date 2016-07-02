acfb46b65d2790950abb5423211fb71d13e3164b
Rafael Armenio <rafael.armenio@gmail.com>
2016-07-02 18:56:53 -0300

    fix

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,6 +11,19 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\ServiceManager\Di',
+    'Zend\Session',
+    'Zend\Mvc\Plugin\Prg',
+    'Zend\Mvc\Plugin\Identity',
+    'Zend\Mvc\Plugin\FlashMessenger',
+    'Zend\Mvc\Plugin\FilePrg',
+    'Zend\Mvc\I18n',
+    'Zend\Mvc\Console',
+    'Zend\Log',
+    'Zend\Form',
+    'Zend\Db',
+    'Zend\Cache',
+    'ZendDeveloperTools',
     'Zend\Router',
     'Zend\Validator',
     'Application',
